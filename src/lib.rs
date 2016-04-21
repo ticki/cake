2ce4626bac388fb24ff34e1d996b7f8eccc7f2e7
ticki <ticki@users.noreply.github.com>
2016-04-21 21:38:54 +0200

    API documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 pub extern crate rayon;
 
+/// Execute a command.
+///
+/// Each argument after the first, denoting the executable, represents a respective argument passed
+/// to the executable.  Optionally, you can specify the working directory by prepending `in "blah";
+/// ` to the argument list.
+///
+/// Examples
+/// ========
+///
+/// To run a command in the current working directory:
+///
+/// ```rust
+/// cmd!("ls", "/"); // 'ls /'
+/// ```
+///
+/// If we want to run this in a custom working directory, we can do:
+///
+/// ```rust
+/// cmd!(in "/"; "ls", ".");
+/// ```
 #[macro_export]
 macro_rules! cmd {
     (in $cd:expr; $cmd:expr $(, $arg:expr)*) => {{
@@ -31,6 +51,9 @@ macro_rules! cmd {
     }};
 }
 
+/// Evaluate N expressions in parallel.
+///
+/// This uses work-stealing as back end.
 #[macro_export]
 macro_rules! par {
     () => {};
@@ -40,6 +63,29 @@ macro_rules! par {
     };
 }
 
+/// Define a build.
+///
+/// This macro takes a block, which resembles the `match` syntax. Left to each of the arms are the
+/// recipe's (compilation unit) name, which is followed by the names of the recipes it depends on,
+/// delimited by `()`.  Right to the arm is the recipe's instructions are placed. This is simply
+/// normal Rust code, defining the recipe.
+///
+/// Additionally, this expands to the main function, which handles arguments as well.
+///
+/// Examples
+/// ========
+///
+/// ```rust
+/// #[macro_use]
+/// extern crate cake;
+///
+/// build! {
+///     start(sodium, libstd) => cmd!("ls"),
+///     sodium(libstd, libextra) => println!("yay"),
+///     libstd() => println!("libstd"),
+///     libextra() => println!("libextra"),
+/// }
+/// ```
 #[macro_export]
 macro_rules! build {
     { $($name:ident($($dep:ident),*) => $cont:expr,)* } => {
